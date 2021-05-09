dc73bfcfc29431117ad6560d62c97616f332c4e5
limwoobin <drogba02@naver.com>
2021-05-09 16:48:54 +0900

    new init

---
+++ jpa_project/src/main/java/jpabook/start/Member.java
@@ -0,0 +1,36 @@
+package jpabook.start;
+
+import javax.persistence.*;  //**
+
+/**
+ * User: HolyEyE
+ * Date: 13. 5. 24. Time: 오후 7:43
+ */
+@Entity
+@Table(name="MEMBER")
+public class Member {
+
+    @Id
+    @Column(name = "ID")
+    private String id;
+
+    @Column(name = "NAME")
+    private String name;
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
