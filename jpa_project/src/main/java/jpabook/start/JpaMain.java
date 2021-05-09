68be674fd009f0bc152361fb2b850cb436959ca6
limwoobin <drogba02@naver.com>
2021-05-09 17:07:18 +0900

    add - 조회,수정,삭제,삽입

--- jpa_project/src/main/java/jpabook/start/JpaMain.java
+++ jpa_project/src/main/java/jpabook/start/JpaMain.java
@@ -17,10 +17,13 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction(); //트랜잭션 기능 획득
 
         try {
-
-
             tx.begin(); //트랜잭션 시작
-            logic(em);  //비즈니스 로직
+
+//            save(em);
+//            find(em);
+//            remove(em);
+            update(em);
+
             tx.commit();//트랜잭션 커밋
 
         } catch (Exception e) {
@@ -33,14 +36,35 @@ public class JpaMain {
         emf.close(); //엔티티 매니저 팩토리 종료
     }
 
-    public static void logic(EntityManager em) {
-
-        String id = "id1";
+    // 등록
+    public static void save(EntityManager em) {
+        String id = "id2";
         Member member = new Member();
         member.setId(id);
         member.setName("ZZZ");
 
-        //등록
         em.persist(member);
+    }
+
+    // 조회
+    public static void find(EntityManager em) {
+        String id = "id1";
+        Member findMember = em.find(Member.class , id);
+        System.out.println("id: " + findMember.getId());
+        System.out.println("name: " + findMember.getName());
+    }
+
+    // 삭제
+    public static void remove(EntityManager em) {
+        String id = "id1";
+        Member findMember = em.find(Member.class , id);
+        em.remove(findMember);
+    }
+
+    // 수정
+    public static void update(EntityManager em) {
+        String id = "id1";
+        Member findMember = em.find(Member.class , id);
+        findMember.setName("updated Name");
     }
 }
