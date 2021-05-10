1889a2ea3996bf1212d2dd286e0fd376e5fd8999
woobeen <drogba02@naver.com>
2021-05-11 04:29:27 +0900

    김영한의 Spring JPA
    - flush 스터디

---
+++ jpa_project/src/main/java/jpabook/start/persistence/FlushTest.java
@@ -0,0 +1,34 @@
+package jpabook.start.persistence;
+
+import jpabook.start.Member;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class FlushTest {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpabook");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            Member member = new Member(200L , "flush Test");
+            em.persist(member);
+            em.flush();
+
+            System.out.println("====================================");
+            tx.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
