ad89cbba85a02ee1f9e65649e9f64deec7a77064
woobeen <drogba02@naver.com>
2021-05-10 17:41:20 +0900

    김영한의 Spring JPA
    - 쓰기지연 저장소 , flush

---
+++ jpa_project/src/main/java/jpabook/start/persistence/PersistenceContextTest2.java
@@ -0,0 +1,44 @@
+package jpabook.start.persistence;
+
+import jpabook.start.Member;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class PersistenceContextTest2 {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpabook");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            Member memberA = new Member();
+            memberA.setId(1L);
+            memberA.setName("A");
+
+            Member memberB = new Member();
+            memberB.setId(2L);
+            memberB.setName("B");
+
+            em.persist(memberA);
+            em.persist(memberB);
+            // 여기까지 insert sql 을 보내지 않는다.
+            // 영속성 컨텍스트의 쓰기지연 저장소에 query가 저장된다
+
+            tx.commit();
+            // commit 시점에 쓰기지연 저장소에 있는 query 들이 flush 되면서 query 가 날라간다
+            // jdbc batch 라 말한다
+        } catch (Exception e) {
+            e.printStackTrace();
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
