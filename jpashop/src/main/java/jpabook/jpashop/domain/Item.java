1bf23b5b6e4b405963ac3d4e1ac3c4210ec594af
woobeen <drogba02@naver.com>
2021-05-12 17:07:51 +0900

    김영한의 Spring JPA
    - jpashop 엔티티 생성

---
+++ jpashop/src/main/java/jpabook/jpashop/domain/Item.java
@@ -0,0 +1,24 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Getter
+@Entity
+public class Item {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "ITEM_ID")
+    private Long id;
+
+    private String name;
+
+    private int price;
+
+    private int stockQuantity;
+}
