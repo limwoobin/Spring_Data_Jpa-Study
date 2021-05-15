7f8cdac92ebcb3a88e3fe4f9834780c8a1dc12a3
limwoobin <drogba02@naver.com>
2021-05-15 17:32:51 +0900

    김영한의 Spring JPA
    - 1:1 연관관계 단방향 , 양방향

---
+++ jpa_project/src/main/java/jpabook/start/ex6/Member.java
@@ -0,0 +1,24 @@
+package jpabook.start.ex6;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+public class Member {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "MEMBER_ID")
+    private Long id;
+
+    @Column(name = "USERNAME")
+    private String username;
+
+    @OneToOne
+    @JoinColumn(name = "LOCKER_ID")
+    private Locker locker;
+}
